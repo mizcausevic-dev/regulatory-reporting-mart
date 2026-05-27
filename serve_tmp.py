e066e982886e06f8374f082c3052c90682ec27bf
fknmiz <150495873+fknmiz@users.noreply.github.com>
2026-05-27 16:42:49 -0400

    Add proof screenshots

--- serve_tmp.py
+++
@@ -1,4 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-import os
-os.chdir(r"C:\Users\chaus\dev\repos\regulatory-reporting-mart\site")
-ThreadingHTTPServer(("127.0.0.1", 8047), SimpleHTTPRequestHandler).serve_forever()
